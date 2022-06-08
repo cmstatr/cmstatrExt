e15564535b3fed44a76396bb107803a437707a84
Stefan Kloppenborg <stefan@kloppenborg.ca>
2022-06-07 21:56:59 -0600

    Added manual text

--- R/cmstatrExt-package.R
+++ R/cmstatrExt-package.R
@@ -7,3 +7,12 @@ NULL
 #' @importFrom Rcpp sourceCpp
 ## usethis namespace: end
 NULL
+
+
+#' @description
+#' `cmstatrExt` is a package that contains statistical methods that can be
+#' used for composite material data, but that are not contained within
+#' CMH-17.
+#' @keywords internal
+#' @docType package
+"_PACKAGE"
